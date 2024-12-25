8a9c626fe51fd27fdd644c1fcce64457c10a8955
Ariela <arielafitiajoharini@gmail.com>
2024-12-25 23:38:50 +0300

    Implement User and Task models with One-to-Many relationship

---
+++ src/main/java/com/ditto/taskmanagement/model/Task.java
@@ -0,0 +1,41 @@
+package com.ditto.taskmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING) // Store Enum as a String in DB
+    @Column(nullable = false)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public enum Status {
+        PENDING, COMPLETED
+    }
+}
---
+++ src/main/java/com/ditto/taskmanagement/model/User.java
@@ -0,0 +1,28 @@
+package com.ditto.taskmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+}
